d7d6ca61bddc469c99b43e5a27a1daee28835d86
Kalita Mikhail <mkalita1991@gmail.com>
2019-02-23 23:00:33 +0300

    initial commit

---
+++ precache-manifest.0958fe899c0b7236aae541d0cb823a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77511caa92db40fe231d",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.77511caa.js"
+  },
+  {
+    "revision": "675ad94f4ef3206216a3",
+    "url": "/rss-mentor-dashboard/static/js/main.675ad94f.chunk.js"
+  },
+  {
+    "revision": "147769358decf0d24e58",
+    "url": "/rss-mentor-dashboard/static/js/1.14776935.chunk.js"
+  },
+  {
+    "revision": "675ad94f4ef3206216a3",
+    "url": "/rss-mentor-dashboard/static/css/main.461aa868.chunk.css"
+  },
+  {
+    "revision": "9f89d6e07c9c9cbbcc28eae0fdb71896",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
